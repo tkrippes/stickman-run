6a976fb5a2e3cf9e3c693551524d1d0005961642
tkrippes <tkrippes@live.de>
2025-02-15 15:58:01 +0100

    feat: change restart level
    restart on last achieved level instead of level 1
    set score on restart accordingly

--- main.gd
+++ main.gd
@@ -5,26 +5,23 @@ enum GameState {
 	GAME_RUNNING,
 	GAME_ENDED
 }
-
 enum Level {
 	LEVEL_1,
 	LEVEL_2,
 	LEVEL_3,
 	LEVEL_4
 }
-
 signal score_updated(score: int)
 signal game_started
 signal level_started(level: int)
 signal game_ended
-
 @export var level_2_unlock_score: int = 25
 @export var level_3_unlock_score: int = 55
 @export var level_4_unlock_score: int = 90
 
 var _game_state: GameState = GameState.GAME_STARTED
-var _level: Level = Level.LEVEL_1
-var _score: int = 0
+var _level: Level          = Level.LEVEL_1
+var _score: int            = 0
 
 
 func _ready() -> void:
@@ -39,16 +36,16 @@ func _input(event: InputEvent) -> void:
 
 func _change_game_state() -> void:
 	match _game_state:
-			GameState.GAME_STARTED:
-				_start_level(Level.LEVEL_1)
-			GameState.GAME_ENDED:
-				_start_game()
+		GameState.GAME_STARTED:
+			_start_level(_level)
+		GameState.GAME_ENDED:
+			_start_game()
 
 
 func _on_player_scored() -> void:
 	_score += 1
 	score_updated.emit(_score)
-	
+
 	if _score == level_2_unlock_score:
 		_start_level(Level.LEVEL_2)
 	elif _score == level_3_unlock_score:
@@ -58,32 +55,33 @@ func _on_player_scored() -> void:
 
 
 func _start_game() -> void:
-	_reset_score()
 	_game_state = GameState.GAME_STARTED
 	game_started.emit()
 
 
-func _reset_score() -> void:
-	_score = 0
+func _set_score(score: int) -> void:
+	_score = score
 	score_updated.emit(_score)
 
 
 func _start_level(level: Level) -> void:
+	_game_state = GameState.GAME_RUNNING
+
 	match level:
 		Level.LEVEL_1:
-			_game_state = GameState.GAME_RUNNING
+			_set_score(0)
 			_level = Level.LEVEL_1
 			level_started.emit(1)
 		Level.LEVEL_2:
-			_game_state = GameState.GAME_RUNNING
+			_set_score(level_2_unlock_score)
 			_level = Level.LEVEL_2
 			level_started.emit(2)
 		Level.LEVEL_3:
-			_game_state = GameState.GAME_RUNNING
+			_set_score(level_3_unlock_score)
 			_level = Level.LEVEL_3
 			level_started.emit(3)
 		Level.LEVEL_4:
-			_game_state = GameState.GAME_RUNNING
+			_set_score(level_4_unlock_score)
 			_level = Level.LEVEL_4
 			level_started.emit(4)
 
