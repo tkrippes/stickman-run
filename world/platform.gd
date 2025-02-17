48e3f735aa4e19d1ccafa6b4a492151fd1918149
tkrippes <tkrippes@live.de>
2025-02-17 21:05:19 +0100

    feat: add ground and platform as foreground scene
    fix platform spawning

--- world/platform.gd
+++ world/platform.gd
@@ -1,14 +1,18 @@
 class_name Platform
 extends StaticBody2D
 
+var _platform_length: float
+
+
+func _ready() -> void:
+	_platform_length = ($Hitbox as CollisionShape2D).position.x * 2
+	
 
 func _on_visible_on_screen_notifier_screen_entered() -> void:
 	var new_platform: Platform = self.duplicate()
-	new_platform.position.x = position.x + 200
+	new_platform.position.x = position.x + _platform_length
 	get_parent().add_child(new_platform)
-	print("created new platform")
 
 
 func _on_visible_on_screen_notifier_screen_exited() -> void:
 	queue_free()
-	print("delete old platform")
