4b3e0d28b475c89b47d94b9869aa742c021206fa
Tom Krippes <tom.krippes@weinig.com>
2025-02-19 10:51:59 +0100

    refactor: rework visibility of player script
    change jump sound to AudioStreamPlayer (without 2D)

--- world/characters/player.gd
+++ world/characters/player.gd
@@ -12,15 +12,15 @@ signal maximum_run_speed_attained
 @export var jump_speed: float = -150.0
 @export var gravity_multiplier: float = 0.4
 
-var run_speed: float
-var animation: AnimatedSprite2D
-var jump_sound: AudioStreamPlayer2D
+var _run_speed: float
+var _animation: AnimatedSprite2D
+var _jump_sound: AudioStreamPlayer
 
 
 func _ready() -> void:
-	run_speed = 0.0
-	animation = $AnimationSprite
-	jump_sound = $JumpSound
+	_run_speed = 0.0
+	_animation = $AnimationSprite
+	_jump_sound = $JumpSound
 
 
 func _physics_process(delta: float) -> void:
@@ -28,22 +28,22 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += _get_gravity() * delta
 	else:
-		jump_sound.stop()
+		_jump_sound.stop()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
 		_jump()
 
-	velocity.x = lerp(velocity.x, run_speed, delta * acceleration)
+	velocity.x = lerp(velocity.x, _run_speed, delta * acceleration)
 
-	if run_speed > 0.0:
+	if _run_speed > 0.0:
 		if is_on_floor():
-			animation.animation = "run"
+			_animation.animation = "run"
 		else:
-			animation.animation = "jump"
-		animation.play()
+			_animation.animation = "jump"
+		_animation.play()
 	else:
-		animation.stop()
+		_animation.stop()
 
 	var _collided := move_and_slide()
 	for index: int in range(get_slide_collision_count()):
@@ -53,17 +53,17 @@ func _physics_process(delta: float) -> void:
 
 
 func start_running() -> void:
-	run_speed = initial_run_speed
-	animation.speed_scale = 1.0
+	_run_speed = initial_run_speed
+	_animation.speed_scale = 1.0
 
 
 func increase_speed() -> void:
-	if (run_speed * speed_multiplier < maximum_run_speed):
-		run_speed *= speed_multiplier
-		animation.speed_scale *= speed_multiplier
-	elif run_speed < maximum_run_speed:
-		animation.speed_scale *= maximum_run_speed / run_speed
-		run_speed = maximum_run_speed
+	if (_run_speed * speed_multiplier < maximum_run_speed):
+		_run_speed *= speed_multiplier
+		_animation.speed_scale *= speed_multiplier
+	elif _run_speed < maximum_run_speed:
+		_animation.speed_scale *= maximum_run_speed / _run_speed
+		_run_speed = maximum_run_speed
 		maximum_run_speed_attained.emit()
 
 
@@ -75,18 +75,18 @@ func _get_gravity() -> Vector2:
 
 
 func _jump() -> void:
-	if is_on_floor() and run_speed > 0.0:
+	if is_on_floor() and _run_speed > 0.0:
 		velocity.y = jump_speed
-		jump_sound.pitch_scale = randf_range(0.9, 1.1)
-		jump_sound.play()
+		_jump_sound.pitch_scale = randf_range(0.9, 1.1)
+		_jump_sound.play()
 
 
 func _die() -> void:
 	hide()
 	
-	run_speed = 0.0
-	animation.animation = "run"
-	animation.stop()
-	jump_sound.stop()
+	_run_speed = 0.0
+	_animation.animation = "run"
+	_animation.stop()
+	_jump_sound.stop()
 	
 	hit.emit()
