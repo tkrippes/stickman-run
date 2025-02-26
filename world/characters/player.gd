3eb4569c4aebf13f2f9f9f1222d54e4e934ebcd3
tkrippes <tkrippes@live.de>
2025-02-26 22:15:20 +0100

    fine-tuning: adjust initial run speed of player.gd
    decrease speed multiplier to have the same difficult as before

--- world/characters/player.gd
+++ world/characters/player.gd
@@ -4,9 +4,9 @@ extends CharacterBody2D
 signal hit
 signal maximum_run_speed_attained
 signal position_updated(position: Vector2)
-@export var initial_run_speed: float = 75.0
+@export var initial_run_speed: float = 80.0
 @export var maximum_run_speed: float = 180.0
-@export var speed_multiplier: float = 1.02
+@export var speed_multiplier: float = 1.019
 @export var acceleration: float = 5.0
 @export var jump_speed: float = -150.0
 @export var gravity_multiplier: float = 0.4
