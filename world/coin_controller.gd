062730664c0891a27edd3d6322b97e4f578bcead
Tom Krippes <tom.krippes@weinig.com>
2025-02-26 12:57:24 +0100

    feat: make spawning of coin more random

--- world/coin_controller.gd
+++ world/coin_controller.gd
@@ -5,9 +5,19 @@ signal coin_collected(points: int)
 @export var coin_scene: PackedScene
 @export var base_position: Vector2 = Vector2(192, 90)
 @export var maximum_height: int = 24
+@export var minimum_random_spawn_rate_multiplier: float = 0.8
+@export var maximum_random_spawn_rate_multiplier: float = 1.2
 @export var points: int = 3
 
 var _player_position: Vector2
+var _spawn_timer: Timer
+var _base_spawn_rate: float
+
+
+func _ready() -> void:
+	_spawn_timer = $SpawnTimer
+	_base_spawn_rate = _spawn_timer.wait_time
+	_set_spawn_rate()
 
 
 func _on_coin_collected() -> void:
@@ -22,6 +32,7 @@ func _on_coin_destroyed() -> void:
 
 
 func _on_spawn_timer_timeout() -> void:
+	_set_spawn_rate()
 	var coin := _create_coin()
 
 	add_child(coin)
@@ -32,11 +43,11 @@ func _on_player_position_updated(player_position: Vector2) -> void:
 
 
 func start_spawning() -> void:
-	($SpawnTimer as Timer).start()
+	_spawn_timer.start()
 
 
 func stop_spawning() -> void:
-	($SpawnTimer as Timer).stop()
+	_spawn_timer.stop()
 
 
 func delete_coins() -> void:
@@ -52,3 +63,9 @@ func _create_coin() -> Coin:
 	_error_code = coin.obstacle_hit.connect(_on_coin_destroyed)
 
 	return coin
+
+
+func _set_spawn_rate() -> void:
+	_spawn_timer.wait_time = randf_range(
+		minimum_random_spawn_rate_multiplier * _base_spawn_rate,
+		maximum_random_spawn_rate_multiplier * _base_spawn_rate)
