483ab1db0b7d5685a77139cea4757b460b0dc167
tkrippes <tkrippes@live.de>
2025-02-28 20:50:26 +0100

    fine-tuning: decrease timeout for platform disappearance
    remove platform hitbox completely instead of disabling it
    platform removal fixes issue where secret item does not fall

--- world/foreground/foreground.gd
+++ world/foreground/foreground.gd
@@ -3,7 +3,7 @@ extends Node2D
 ## The offset of the platform from the player
 @export var position_offset: float = - 24.0
 ## The timeout before the platform disappears after the player stops.
-@export var platform_disappear_timeout: float = 6.0
+@export var platform_disappear_timeout: float = 5.0
 
 
 func _on_player_position_updated(_position: Vector2) -> void:
@@ -13,4 +13,4 @@ func _on_player_position_updated(_position: Vector2) -> void:
 func _on_player_stopped(_player_position_x: int) -> void:
 	await get_tree().create_timer(platform_disappear_timeout).timeout
 	($Ground as Parallax2D).hide()
-	($Platform/Hitbox as CollisionShape2D).set_disabled(true)
+	($Platform/Hitbox as CollisionShape2D).queue_free()
